7ea653b0be098d5d32c46c5024f0dd4cf42e16a2
Daniel <danilagalakov3@gmail.com>
2023-11-09 22:42:48 +0600

    norel

--- src/main/Home/Home.jsx
+++ src/main/Home/Home.jsx
@@ -1,54 +1,44 @@
-import { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Style from '../../styles/Main/Home/Home.module.css';
 
 const Home = () => {
     document.title = 'Todo';
+    const [todoList, setTodoList] = useState([]);
     const inputRef = useRef();
 
-    let todoArray;
-    const sendForm = () => {
-        if(inputRef) {
-            if(inputRef.current.value.length > 3) {
-                const check = JSON.parse(localStorage.getItem('todo'));
-                if(check !== null) todoArray = check;
-                else todoArray = [];
-            
-                const textLS =
-                {
-                    text: inputRef.current.value,
-                };
-        
-                todoArray.push(textLS);
-                window.localStorage.setItem('todo', JSON.stringify(todoArray));
-                JSON.parse(localStorage.getItem('todo'))
-            }
+    useEffect(() => {
+        const savedTodoList = JSON.parse(localStorage.getItem('todo'));
+        if (savedTodoList) {
+            setTodoList(savedTodoList);
         }
+    }, []);
 
-        inputRef.current.value = '';
-    }
-
-    const parseTodo = JSON.parse(localStorage.getItem('todo'));
-
+    const sendForm = () => {
+        const inputText = inputRef.current.value.trim();
+        if (inputText.length > 0) {
+            const newTodoList = [...todoList, inputText];
+            setTodoList(newTodoList);
+            localStorage.setItem('todo', JSON.stringify(newTodoList));
+            inputRef.current.value = '';
+        }
+    };
 
     return (
         <main>
-        <form>
             <div className={Style.input}>
-                <input ref={inputRef} type="text" placeholder="Введите задачу"/>
+                <input ref={inputRef} type="text" placeholder="Введите задачу" />
                 <button onClick={sendForm} type="submit">Добавить</button>
             </div>
-        </form>
             <div className={Style.todo_list}>
-                {parseTodo !== null ? 
-                    parseTodo.map((todo, i) => (
-                        <div key={i}>                            
+                {todoList.map((todo, index) => (
+                    <div key={index}>
                         <div className={Style.todo_list_block}>
                             <div className={Style.todo_list_block__id}>
-                                <p>{i + 1}</p>
+                                <p>{index + 1}</p>
                             </div>
 
                             <div className={Style.todo_list_block__text}>
-                                <p>{todo.text}</p>
+                                <p>{todo}</p>
                             </div>
 
                             <div onClick={()=>{
@@ -56,39 +46,36 @@ const Home = () => {
                             if(newParams) {
                                 if(newParams.length < 3) return false;
                             } else return false;
-                            let todoData = {
-                                text: newParams
-                            }
+                            let todoData = newParams;
+
                             let todoRew = JSON.parse(localStorage.getItem('todo'));
-                            todoRew[i] = todoData;
+                            todoRew[index] = todoData;
 
                             window.localStorage.setItem('todo', JSON.stringify(todoRew));
                             window.location.reload();
 
+
+
                             }} className={Style.todo_list_block__rewrite}>
                                 <button><img src="./images/pictures/edit.svg" alt="edit" width="16"/></button>
                             </div>
+
+                            
                             <div onClick={()=>{
 
-                                let todoRew = JSON.parse(localStorage.getItem('todo'));
-                                let todoData = {
-                                    text: todoRew[i].text
-                                }
-                                todoRew[i] = todoData;
-                                todoRew.splice(i, 1);
-                                window.localStorage.setItem('todo', JSON.stringify(todoRew));
-                                window.location.reload();
+                            let todoRew = JSON.parse(localStorage.getItem('todo'));
+                            let todoData = todoRew[index].text
+                            todoRew[index] = todoData;
+                            todoRew.splice(index, 1);
+                            window.localStorage.setItem('todo', JSON.stringify(todoRew));
+                            window.location.reload();
 
                             }} className={Style.todo_list_block__delete}>
-                                <button><img src="./images/pictures/close.svg" alt="delte" width="16"/></button>
+                            <button><img src="./images/pictures/close.svg" alt="delete" width="16"/></button>
                             </div>
                         </div>
-                        </div>
-                    ))
-                : <div className={Style.noRes}><p>У вас нет текущих задач.</p></div>}
-                {localStorage.getItem('todo') === '[]' &&
-                    <div className={Style.noRes}><p>У вас нет текущих задач.</p></div>
-                }
+                    </div>
+                ))}
             </div>
         </main>
     );
